f8f464ea8f4bee7b646d23b90af960779da552a1
sgarcia <stevensantosgarcia@gmail.com>
2025-08-11 15:15:07 -0500

    No parsing

--- app/Main.hs
+++ app/Main.hs
@@ -4,9 +4,24 @@
 
 module Main (main) where
 
+import Control.Monad (replicateM_)
 import qualified Data.ByteString.Char8 as BC
-import Network.Simple.TCP (serve, HostPreference(HostAny), closeSock, send)
+import qualified Data.ByteString as B
+import Data.Char (ord)
+import qualified Data.Map as M
+import Data.Word (Word8)
+import Network.Simple.TCP (serve, HostPreference(HostAny), closeSock, send, recv)
 import System.IO (hPutStrLn, hSetBuffering, stdout, stderr, BufferMode(NoBuffering))
+
+
+charCodesMap :: M.Map Char Word8
+charCodesMap =
+    M.fromList
+        [ (' ', fromIntegral (ord ' ') :: Word8)
+        , (':', fromIntegral (ord ':') :: Word8)
+        , ('\r', fromIntegral (ord '\r') :: Word8)
+        , ('\n', fromIntegral (ord '\n') :: Word8)
+        ]
 
 main :: IO ()
 main = do
@@ -21,5 +36,18 @@ main = do
     putStrLn $ "Redis server listening on port " ++ port
     serve HostAny port $ \(socket, address) -> do
         putStrLn $ "successfully connected client: " ++ show address
-        send socket $ BC.pack "+PONG\r\n" 
-        --closeSock socket
+
+        -- read chunk of data into a buffer
+        buffer <- recv socket 4096
+        case buffer of
+            Just request -> do
+                --let cmds = B.splitWith (\x -> x == charCodesMap M.! '\r' || x ==  charCodesMap M.! '\n') request
+                let cmdCount = B.count (charCodesMap M.! '\r') request
+                print cmdCount
+                replicateM_ cmdCount (send socket $ BC.pack "+PONG\r\n")
+            Nothing -> closeSock socket
+            --send socket $ BC.pack "+PONG\r\n" 
+            ----closeSock socket
+            --putStrLn $ "request: " ++ show request
+            --send socket $ BC.pack "+PONG\r\n" 
+            ----closeSock socket
